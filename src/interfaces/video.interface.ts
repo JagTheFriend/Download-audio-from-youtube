6045524009a7e6fad6fef9d68daf0b4d38e271c8
JagTheFriend <jagthefriend12@gmail.com>
2022-02-20 19:14:24 +0000

    Created another interface to represent the data being sent to the client

--- src/interfaces/video.interface.ts
+++ src/interfaces/video.interface.ts
@@ -1,23 +1,31 @@
 export interface VideoFormat {
-    nextPageToken: string;
-    videoId: string;
-    title: string;
-    description: string;
-    thumbnailLink: string;
+  nextPageToken: string;
+  videoId: string;
+  title: string;
+  description: string;
+  thumbnailLink: string;
 }
 
 export interface ResponseReceived {
-    nextPageToken: string;
-    items: [
-        {
-            id: { videoId: string };
-            snippet: {
-                title: string;
-                description: string;
-                thumbnails: {
-                    medium: { url: string };
-                };
-            };
-        },
-    ];
+  nextPageToken: string;
+  items: [
+    {
+      id: { videoId: string };
+      snippet: {
+        title: string;
+        description: string;
+        thumbnails: {
+          medium: { url: string };
+        };
+      };
+    },
+  ];
+}
+
+export interface DataToSend {
+  nextPageToken: string;
+  videoId: string;
+  title: string;
+  description: string;
+  thumbnailLink: string;
 }
